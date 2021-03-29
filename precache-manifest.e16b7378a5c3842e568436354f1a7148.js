675dad903c423f02b2911e1ddcc0aa072e8fcd1a
Alex <4853941@gmail.com>
2021-03-29 12:02:56 +0300

    deploy

---
+++ precache-manifest.e16b7378a5c3842e568436354f1a7148.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005b2787a92342d7a53a",
+    "url": "/oui-front/css/app.606455c3.css"
+  },
+  {
+    "revision": "7cec0f41f2812b2e2c7b",
+    "url": "/oui-front/css/chunk-vendors.b45ed4c9.css"
+  },
+  {
+    "revision": "3be667199f5a55df7523c5150c21437d",
+    "url": "/oui-front/img/oui-logo.3be66719.png"
+  },
+  {
+    "revision": "56136cfb02fb3bd3a5095886d94df4dd",
+    "url": "/oui-front/index.html"
+  },
+  {
+    "revision": "005b2787a92342d7a53a",
+    "url": "/oui-front/js/app.9abebae4.js"
+  },
+  {
+    "revision": "7cec0f41f2812b2e2c7b",
+    "url": "/oui-front/js/chunk-vendors.6aeee6d2.js"
+  },
+  {
+    "revision": "3aab2bf3a2df5677e9ca159fb438fe6e",
+    "url": "/oui-front/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/oui-front/robots.txt"
+  }
+]);
